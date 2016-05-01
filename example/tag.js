f0e0d83f857eed8d8d73dc8d126624648d82c72e
adz5a <adz5a@gmail.com>
2016-05-02 00:00:05 +0200

    add some comments

--- example/tag.js
+++ example/tag.js
@@ -3,6 +3,7 @@ var tag    = require( "./../tag" );
 var global = require( "./../global" );
 var __sto  = global.__sto;
 
+// bootstrap the tag with the current page context
 tag( function () {
     return {
         "retailPage": "accueil/test",
@@ -10,6 +11,7 @@ tag( function () {
     };
 } );
 
+// declares that you will load a SK
 __sto.deliv( {
     "SK": {
         "id": "scriptId",
@@ -18,11 +20,12 @@ __sto.deliv( {
     }
 } );
 
+// effectively load the format through the load interface
 __sto.load( {
     "id"          : "scriptId",
     "dependencies": [
         {
-            "id"      : "template.searchandising",
+            "id"      : "template.searchandising", // declares a dep on templare.searchandising, demands it to be loaded before format is executed
             "settings": {
                 ".sto-searchandising-container": {
                     "background-color": "blue"
@@ -39,9 +42,21 @@ __sto.load( {
     }
 } );
 
+// register the module
+// if multiple calls are made with teh same ids, they will be ignored
+// as the implementation relies on promises resolved with the module object
 __sto.register( {
     "type"   : "template",
     "name"   : "searchandising",
+
+    /**
+     *
+     * @param global - global namespace whatever that means
+     * @param context - an event emitter/notifier used to communicate with the rest of the page
+     *                  could be used to channel DOM events
+     * @param settings - runtime format settings, they will be applied before the format is executed
+     * @returns {{run: "run", remove: "remove"}} // exposes two methods
+     */
     "builder": function ( global, context, settings ) {
         
         console.log( "hello man" );
@@ -69,7 +84,11 @@ __sto.register( {
             wrapper.find( selector ).css( settings[ selector ] );
             
         } );
-        
+
+        // exposes two methods :
+        // run : needs an explicit call on template.run() to effectively launch the module
+        // remove : a simple map to $object.remove, used to remove the DOM elements from the document
+        // Idea : add a destroy method to remove all possible event listeners / variables to allow for garbage collecting
         return {
             "run"   : function () {
                 
